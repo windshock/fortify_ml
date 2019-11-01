1627a2176958a4626960c37b8660a939ff824a46
windshock <windshock@gmail.com>
2019-11-01 13:57:36 +0900

    K값을 결정하기 위한 테스트 데이터 및 로직 추가

--- main.py
+++ main.py
@@ -6,28 +6,47 @@ from sklearn.preprocessing import normalize
 from sklearn.cluster import KMeans
 import math
 import pandas as pd
+import numpy as np
 import os
 
 base_dir = './data/'
-excel_file = 'fortify_ml2.xlsx'
+excel_file = 'fortify_ml.xlsx'
+test_file = 'test.xlsx'
 excel_dir = os.path.join(base_dir,excel_file)
+test_dir = os.path.join(base_dir,test_file)
 df_from_excel=pd.read_excel(excel_dir,sheet_name='Sheet1',header=0,names=['Fortify_NodeType','k16','k18','k21','manual','vul?'],dtype={'Fortify_NodeType':str,'kmeans':str,'manual':str},na_values='NaN',thousands=',',comment='#')
-docs = df_from_excel['Fortify_NodeType'].values
-k=math.ceil(len(docs)*0.34615)
+df_from_test=pd.read_excel(test_dir,sheet_name='Sheet1',header=0,names=['Fortify_NodeType','k16','k18','k21','manual','vul?'],dtype={'Fortify_NodeType':str,'kmeans':str,'manual':str},na_values='NaN',thousands=',',comment='#')
+docs = df_from_test.append(df_from_excel)['Fortify_NodeType'].values
 
-# vectorizing
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(docs)
+k=math.ceil(len(docs)/2)
 
-# L2 normalizing
-X = normalize(X, norm='l2')
+#find k
+for i in range(k,0,-1):
+    # vectorizing
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(docs)
 
-# training k-means
-kmeans = KMeans(n_clusters=k).fit(X)
+    # L2 normalizing
+    X = normalize(X, norm='l2')
+
+    # training k-means
+    kmeans = KMeans(n_clusters=i).fit(X)
+
+    # check test data
+    if (kmeans.labels_[4] == kmeans.labels_[5]):
+        k=i+1
+        kmeans = KMeans(n_clusters=k).fit(X)
+        break
+    if (kmeans.labels_[0] == kmeans.labels_[1]) & (kmeans.labels_[2] == kmeans.labels_[3]):
+        k=i
+        break
+
+if k == math.ceil(len(docs)*0.5):
+    print("this is fail!!!")
 
 # trained labels and cluster centers
 labels = kmeans.labels_
 centers = kmeans.cluster_centers_
 
 print(labels)
-print(math.ceil((len(docs)*0.4)))
+print("k is ", k)
