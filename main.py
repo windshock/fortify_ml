e4c615953801b9f406b3ff3f8f34426e7c54c09f
windshock <windshock@gmail.com>
2019-11-07 09:58:27 +0900

    kmeans 그래프 그리기 실패

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 from sklearn.metrics import silhouette_score
+import seaborn as sns
 
 
 base_dir = './data/'
@@ -46,14 +47,19 @@ plt.xlabel('k')
 plt.title('Elbow curve')
 plt.show()
 """
+
+# vectorizing
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(docs)
+df = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names())
+#reduced_data = X.toarray()
+
+# L2 normalizing
+X = normalize(X, norm='l2')
+
 #find k
 for i in range(k,0,-1):
-    # vectorizing
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(docs)
 
-    # L2 normalizing
-    X = normalize(X, norm='l2')
 
     # training k-means
     kmeans = KMeans(n_clusters=i).fit(X)
@@ -78,3 +84,31 @@ centers = kmeans.cluster_centers_
 
 print(labels)
 print("k is ", k)
+
+#그래프 그리다 실패 어찌 그리나?
+"""
+#df = pd.DataFrame(X)
+#df['category'] = labels
+#sns.lmplot('x','y',data=df,fit_reg=False,scatter_kws={"s":200},hue="category")
+#plt.matshow(df.corr())
+#plt.xticks(range(len(df.columns)), df.columns)
+#plt.yticks(range(len(df.columns)), df.columns)
+#y_kmeans = kmeans.predict(X)
+#plt.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='viridis')
+#centers = kmeans.cluster_centers_
+#plt.scatter(centers[:, ], c='black', s=200, alpha=0.5);
+#plt.show()
+
+for i in range(k):
+    # select only data observations with cluster label == i
+    ds = X[np.where(labels==i)]
+    # plot the data observations
+    print("a")
+    plt.plot(ds[:,0],ds[:,1],'o')
+    # plot the centroids
+    lines = plt.plot(centers[i,0],centers[i,1],'kx')
+    # make the centroid x's bigger
+    plt.setp(lines,ms=15.0)
+    plt.setp(lines,mew=2.0)
+plt.show()
+"""
